678c770e987125332169c24730c48aed10aa2c46
Gabriel <Gabriel@DESKTOP-OFSS190>
2025-03-06 00:21:51 -0300

    Add project files.

---
+++ beecrowd/Program.cs
@@ -0,0 +1,13 @@
+﻿int numero, i;
+
+Console.WriteLine("Digite um numero inteiro: ");
+numero = int.Parse(Console.ReadLine());
+
+for (i = 0; i < numero; i++)
+{
+    if (i % 2 == 1)
+    {
+        Console.WriteLine(i);
+    }
+
+}
